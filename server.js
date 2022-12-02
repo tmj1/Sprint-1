1ab1562d1b2d6088d0cfec42b1efb97d8b4bf428
tmj1 <tmj1@mail.ru>
2022-12-02 16:03:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 const { PORT = 3000 } = process.env;
-const pathName = path.join(__dirname, '../Sprint 2 git/dist/index.html');
+const pathName = path.join(__dirname, './dist/index.html');
 
 app.use(express.static('./dist'));
 
